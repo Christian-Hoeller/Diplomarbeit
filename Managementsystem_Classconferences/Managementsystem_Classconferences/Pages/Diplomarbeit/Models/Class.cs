a5e22cd3165a6cbfe0e22f33f5d2d4135974defb
Christian-Hoeller <ch.hoeller14@gmail.com>
2019-07-30 12:07:55 +0200

    Added Table to show Teachers of current Class

---
+++ Managementsystem_Classconferences/Managementsystem_Classconferences/Pages/Diplomarbeit/Models/Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Managementsystem_Classconferences.Pages.Diplomarbeit.Models
+{
+    public class Class
+    {
+        public string ClassName { get; set; }
+        public string HeadOfDepartment { get; set;}
+        public string FormTeacher { get; set; }
+        public List<Teacher> Teachers { get; set; }
+    }
+}
---
+++ Managementsystem_Classconferences/Managementsystem_Classconferences/Pages/Diplomarbeit/Models/Teacher.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Managementsystem_Classconferences.Pages.Diplomarbeit.Models
+{
+    public class Teacher
+    {
+        [Key]
+        public string ID { get; set; }
+        public string Name { get; set; }
+        public string Name_Short { get; set; }
+    }
+}
