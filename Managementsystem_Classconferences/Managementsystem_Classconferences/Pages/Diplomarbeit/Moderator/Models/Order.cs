da540f92d52efd720b512a8375b52c9355b784cc
Christian-Hoeller <ch.hoeller14@gmail.com>
2019-08-06 16:08:26 +0200

    added site for redirecting

---
+++ Managementsystem_Classconferences/Managementsystem_Classconferences/Pages/Diplomarbeit/Moderator/Models/Order.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Managementsystem_Classconferences.Pages.Diplomarbeit.Moderator.Models
+{
+    public class Order
+    {
+        [Key]
+        public string Room { get; set; }
+        public string Room_only { get; set; }
+    }
+}
